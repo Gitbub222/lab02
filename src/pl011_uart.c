bfb6448c7da7622014906cf1504be1f16e4a4993
Baraka David Mulungula <bmulungula@gmail.com>
2023-10-12 17:35:16 -0400

    Update pl011_uart.c

--- src/pl011_uart.c
+++ src/pl011_uart.c
@@ -1,39 +1,36 @@
 #include "utils.h"
 #include "peripherals/pl011_uart.h"
 #include "peripherals/gpio.h"
-#include <stdint.h>
 
-void uart_send(char ch)
+void uart_send(char c)
 {
-    /*Transmit FIFO is full*/
-    while (get32(UARTFR) & (1<<5))
-    {
-        ;
-    }
-    put32(UARTDR, ch);
+	// Wait until the UART FIFO is not full
+	while (get32(UARTFR) & (1 << 5)); // Wait until TXFF (Transmit FIFO Full) is clear
+
+	// Send the character
+	put32(UARTDR, c);
 }
 
 char uart_recv(void)
 {
-    /*Receive FIFO is empty*/
-    while (get32(UARTFR) & (1<<4))
-    {
-        ;
-    }
-    return (get32(UARTDR) & 0xFF);
+	// Wait until the UART FIFO has data
+	while (get32(UARTFR) & (1 << 4)); // Wait until RXFE (Receive FIFO Empty) is clear
+
+	// Return the received character
+	return (char)(get32(UARTDR) & 0xFF);
 }
 
-void uart_send_string(char* str)
+void uart_send_string(char *str)
 {
-    for (int i = 0; str[i] != '\0'; i++)
-    {
-       uart_send((char)str[i]);
-    }
+	for (int i = 0; str[i] != '\0'; i++)
+	{
+		uart_send((char)str[i]);
+	}
 }
 
 void uart_init(void)
 {
-    unsigned int selector;
+	unsigned int selector;
 
     /*set gpio function*/
     selector = get32(GPFSEL1);
@@ -50,11 +47,20 @@ void uart_init(void)
     delay(150);
     put32(GPPUDCLK0, 0);
 
+	put32(UARTCR, 0);                      // disable uart
 
-    /*Initializing PL011 uart*/
-    put32(UARTCR, 0);                      // disable uart
-    put32(UARTIBRD, 26);                   // set baudrate = 115200
-    put32(UARTFBRD, 3);
-    put32(UARTLCR_H, (1<<4) | (3<<5));      // 8bits and enable FIFO
-    put32(UARTCR, (1 | (3<<8)));           // enable uart, rx, tx
+	// Set the baud rate
+	// 48MHz UART clock; baud rate 115200:
+	// Baud rate divisor = 
+	// UART clock / (16 * Baud rate) = 48,000,000 / (16 * 115200) = 26.0416667
+	// Fractional part = round(0.0416667 * 64) = 3
+	put32(UARTIBRD, 26); // Integer part
+	put32(UARTFBRD, 3);	 // Fractional part
+
+	// Configure the UART line control register
+	// Word length (8 bits), enable FIFOs
+	put32(UARTLCR_H, (1<<4) | (3<<5));
+
+	// Enable the UART; enable transmit and receive
+	put32(UARTCR, (1 | (3<<8)));
 }
